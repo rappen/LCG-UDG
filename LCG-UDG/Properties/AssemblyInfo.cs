0e751f4a2d56849270f3ff39d376863d2b660fc4
Jonas Rapp <jonas@jonasr.app>
2023-05-30 11:21:02 +0200

    Update version number

--- LCG-UDG/Properties/AssemblyInfo.cs
+++ LCG-UDG/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Latebound Constants Generator and UML Diagram Generator")]
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jonas Rapp, Sweden")]
 [assembly: AssemblyProduct("LCG-UDG")]
-[assembly: AssemblyCopyright("Copyright © Jonas Rapp 2017-2021")]
+[assembly: AssemblyCopyright("Copyright © Jonas Rapp 2017-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -24,12 +24,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2021.1.2")]
-[assembly: AssemblyFileVersion("1.2021.1.2")]
+[assembly: AssemblyVersion("1.2023.5.901")]
+[assembly: AssemblyFileVersion("1.2023.5.901")]
